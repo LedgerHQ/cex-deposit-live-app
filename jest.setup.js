6233e58581b43d11fc6abead00305c302118968b
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-09-12 11:45:38 +0200

    Init Tests ✅

---
+++ jest.setup.js
@@ -0,0 +1,9 @@
+// Learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom"
+
+import { ArrayBuffer, TextDecoder, TextEncoder, Uint8Array } from "util"
+
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
+global.ArrayBuffer = ArrayBuffer
+global.Uint8Array = Uint8Array
