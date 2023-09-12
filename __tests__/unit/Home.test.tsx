2efe8fde73fdc77cae4755f3d6c9798826e350f2
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-09-12 15:02:44 +0200

    fix coverage

--- __tests__/unit/Home.test.tsx
+++ __tests__/unit/Home.test.tsx
@@ -2,17 +2,17 @@
  * @jest-environment jsdom
  */
 
-import Home from "@/src/app/page"
-import { render, screen } from "@/tools/test.tools"
+import Home from "@/src/app/page";
+import { render, screen } from "@/tools/test.tools";
 
 describe("Home", () => {
   it("renders a heading", () => {
-    render(<Home />)
+    render(<Home />);
 
     const heading = screen.getByRole("heading", {
       name: /Docs/i,
-    })
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(heading).toBeInTheDocument();
+  });
+});
