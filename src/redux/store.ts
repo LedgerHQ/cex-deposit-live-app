574d4f3d839d431757bf79ea3f114dc22d61d51c
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-09-20 10:52:54 +0200

    Setup store for tests

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { PreloadedState, combineReducers, configureStore } from "@reduxjs/toolkit";
 import exampleReducer from "./reducers/example-slice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
@@ -6,13 +6,22 @@ export const appReducer = combineReducers({
   exampleReducer,
 });
 
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: appReducer,
+    preloadedState,
+    devTools: process.env.NODE_ENV !== "production",
+  });
+};
+
 export const store = configureStore({
   reducer: appReducer,
   devTools: process.env.NODE_ENV !== "production",
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof appReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
